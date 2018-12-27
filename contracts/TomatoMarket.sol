d1289ea23cfde84435ccacb81295289dade98996
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-27 01:27:23 +0100

    Add getTomato function

--- contracts/TomatoMarket.sol
+++ contracts/TomatoMarket.sol
@@ -26,7 +26,15 @@ contract TomatoMarket {
     }
 
     function addTomato(string memory displayName, string memory description, uint8 price, TomatoStatus status, string memory photoHash) public {
+        tomatoes[tomatoCount] = Tomato(tomatoCount, displayName, description, price, status, photoHash);
+        tomatoIds.push(tomatoCount);
+        tomatoCount++;
+        emit TomatoAdded("Tomato Added");
+    }
 
+    function getTomato(uint8 tomatoId) public view returns (string memory displayName, string memory description, uint8 price, string memory photoHash) {
+        Tomato memory currentTomato = tomatoes[tomatoId];
+        return (currentTomato.displayName, currentTomato.description, currentTomato.price, currentTomato.photoHash);
     }
 
 }
